828880c84e5b7a41812aeb0cf9b6df38deb51891
Florence Stadelmann <flst8788@pelle1.uppmax.uu.se>
2026-04-08 11:52:02 +0200

    Add FastQC script for quality control of raw whole genome reads

---
+++ code/01_preprocessing/1_fastqc_WG_Raw.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 2
+#SBATCH -t 00:30:00
+#SBATCH -J fastqc_WG
+
+# Load modules
+module load FastQC
+
+# Run FastQC
+fastqc -t 2 /home/flst8788/Genome-Analysis-1MB462/data/raw_data/CRR809859_*.fq.gz \
+-o /home/flst8788/Genome-Analysis-1MB462/analysis/01_preprocessing/fastqc_raw/
