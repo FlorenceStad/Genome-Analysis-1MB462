c9e2d40e6f1b2657ee7178f3e773cf05185faec0
Florence Stadelmann <flst8788@pelle1.uppmax.uu.se>
2026-04-09 13:46:11 +0200

    Updated analysis time due to previously abrupted runs

--- code/01_preprocessing/1_fastqc_WG_Raw.sh
+++ code/01_preprocessing/1_fastqc_WG_Raw.sh
@@ -3,7 +3,7 @@
 #SBATCH -A uppmax2026-1-61
 #SBATCH -p pelle
 #SBATCH -c 2
-#SBATCH -t 00:30:00
+#SBATCH -t 01:00:00
 #SBATCH -J fastqc_WG
 #SBATCH -o logs/slurm-%j.out
 
--- code/01_preprocessing/2_trimmomatic_WG.sh
+++ code/01_preprocessing/2_trimmomatic_WG.sh
@@ -3,7 +3,7 @@
 #SBATCH -A uppmax2026-1-61
 #SBATCH -p pelle
 #SBATCH -c 2
-#SBATCH -t 02:00:00
+#SBATCH -t 06:00:00
 #SBATCH -J trim_WG
 #SBATCH -o logs/slurm-%j.out
 
