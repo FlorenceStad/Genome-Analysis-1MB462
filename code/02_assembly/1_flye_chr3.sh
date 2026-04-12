a5d54d6694a7f85d275399254f68339e9f9afbbd
Florence Stadelmann <flst8788@pelle1.uppmax.uu.se>
2026-04-12 14:39:24 +0200

    created script for assembly with flye

---
+++ code/02_assembly/1_flye_chr3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 2
+#SBATCH -t 24:00:00
+#SBATCH -J flye_chr3
+#SBATCH -o logs/slurm-%j.out
+
+# Load module
+module load flye
+
+# Define input and output paths
+INPUT=/home/flst8788/Genome-Analysis-1MB462/data/raw_data/chr3_clean_nanopore.fq.gz
+
+OUTDIR=/proj/uppmax2026-1-61/flst8788/Genome-Analysis-1MB462/analysis/02_assembly/flye_chr3
+
+# Create output directory 
+mkdir -p $OUTDIR
+
+# Run Flye assembly
+flye \
+  --nano-raw $INPUT \
+  --out-dir $OUTDIR \
+  --threads 2
