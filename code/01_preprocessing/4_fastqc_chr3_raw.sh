e3bd32915b461cdc34858995e57f3dacf7469cac
Florence Stadelmann <flst8788@pelle1.uppmax.uu.se>
2026-04-12 12:53:32 +0200

    added absolute paths for robustness

---
+++ code/01_preprocessing/4_fastqc_chr3_raw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 2
+#SBATCH -t 01:00:00
+#SBATCH -J fastqc_chr3_raw
+#SBATCH -o logs/slurm-%j.out
+
+module load FastQC
+
+mkdir -p /home/flst8788/Genome-Analysis-1MB462/analysis/01_preprocessing/fastqc_raw_chr3
+
+fastqc -t 2 \
+/home/flst8788/Genome-Analysis-1MB462/data/raw_data/chr3_illumina_R1.fastq.gz \
+/home/flst8788/Genome-Analysis-1MB462/data/raw_data/chr3_illumina_R2.fastq.gz \
+-o /home/flst8788/Genome-Analysis-1MB462/analysis/01_preprocessing/fastqc_raw_chr3/
