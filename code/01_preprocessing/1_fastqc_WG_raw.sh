ea419a896ffdcaf059e0932e66dc8713496eed65
Florence Stadelmann <flst8788@pelle1.uppmax.uu.se>
2026-04-12 12:32:27 +0200

    renamed the code file and the output file for consistency

--- code/01_preprocessing/1_fastqc_WG_raw.sh
+++ code/01_preprocessing/1_fastqc_WG_raw.sh
@@ -12,4 +12,4 @@ module load FastQC
 
 # Run FastQC
 fastqc -t 2 /home/flst8788/Genome-Analysis-1MB462/data/raw_data/CRR809859_*.fq.gz \
--o /home/flst8788/Genome-Analysis-1MB462/analysis/01_preprocessing/fastqc_raw/
+-o /home/flst8788/Genome-Analysis-1MB462/analysis/01_preprocessing/fastqc_raw_wg/
