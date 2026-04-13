231a2c3952c315e20f6a9ab907b637724d1c9546
Florence Stadelmann <flst8788@pelle1.uppmax.uu.se>
2026-04-13 20:28:01 +0200

    created pilon batchcode

--- code/02_assembly/3_pilon_chr3.sh
+++ code/02_assembly/3_pilon_chr3.sh
@@ -1,1 +1,43 @@
----
+#!/bin/bash -l
+
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 2
+#SBATCH -t 24:00:00
+#SBATCH --mem=32G
+#SBATCH -J pilon_chr3
+#SBATCH -o logs/slurm-%j_pilon.out
+
+module load Pilon
+module load samtools
+
+# -----------------------
+# INPUT PATHS
+# -----------------------
+
+ASSEMBLY=/home/flst8788/Genome-Analysis-1MB462/analysis/02_assembly/flye_chr3/assembly.fasta
+
+BAM=/home/flst8788/Genome-Analysis-1MB462/analysis/02_assembly/bwa_chr3/aln.sorted.bam
+
+OUTDIR=/home/flst8788/Genome-Analysis-1MB462/analysis/02_assembly/pilon_chr3
+
+mkdir -p $OUTDIR
+mkdir -p logs
+
+# -----------------------
+# INDEX BAM (safety step)
+# -----------------------
+samtools index $BAM
+
+# -----------------------
+# RUN PILON
+# -----------------------
+
+java -Xmx30G -jar $PILON_JAR \
+  --genome $ASSEMBLY \
+  --frags $BAM \
+  --output pilon_chr3 \
+  --outdir $OUTDIR \
+  --threads 4 \
+  --changes \
+  --vcf
