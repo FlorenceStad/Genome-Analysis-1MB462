805716f577d3eff507797a0778ca26021f7e9a7e
Florence Stadelmann <flst8788@pelle1.uppmax.uu.se>
2026-04-13 21:55:17 +0200

    added script for busco

--- code/02_assembly/5_busco_chr3.sh
+++ code/02_assembly/5_busco_chr3.sh
@@ -1,2 +1,28 @@
----
+#!/bin/bash -l
 
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 2
+#SBATCH -t 01:00:00
+#SBATCH --mem=16G
+#SBATCH -J busco_chr3
+#SBATCH -o logs/slurm-%j_busco.out
+
+module purge
+module load BUSCO/5.7.0
+
+# Input (use polished assembly!)
+ASSEMBLY=/home/flst8788/Genome-Analysis-1MB462/analysis/02_assembly/pilon_chr3/pilon_chr3.fasta
+
+# Output directory
+OUTDIR=/home/flst8788/Genome-Analysis-1MB462/analysis/02_assembly/busco_chr3
+mkdir -p $OUTDIR
+
+# Run BUSCO
+busco \
+  -i $ASSEMBLY \
+  -l embryophyta_odb10 \
+  -o busco_chr3 \
+  --out_path $OUTDIR \
+  -m genome \
+  -c 2
