b914d6a1b12bd02fe8c2ea2fc530527d66cc2c10
Florence Stadelmann <flst8788@pelle1.uppmax.uu.se>
2026-04-13 15:04:28 +0200

    renamed trimming of raw chromsome 3 for consistency

--- code/01_preprocessing/3_trimmomatic_chr3.sh
+++ code/01_preprocessing/3_trimmomatic_chr3.sh
@@ -13,8 +13,8 @@ module load Trimmomatic
 R1=/home/flst8788/Genome-Analysis-1MB462/data/raw_data/chr3_illumina_R1.fastq.gz
 R2=/home/flst8788/Genome-Analysis-1MB462/data/raw_data/chr3_illumina_R2.fastq.gz
 
-# Output directory (your project storage)
-OUTDIR=/proj/uppmax2026-1-61/flst8788/Genome-Analysis-1MB462/analysis/01_preprocessing/trimmed_chr3
+# Output directory
+OUTDIR=/home/flst8788/Genome-Analysis-1MB462/analysis/01_preprocessing/trimmed_chr3
 
 mkdir -p $OUTDIR
 
