11752a792de64bfcc07e8ae374ec2d0621d1174f
Florence Stadelmann <flst8788@pelle2.uppmax.uu.se>
2026-04-14 11:10:14 +0200

    added script for running preprocessing of hic and nanopore

--- code/01_preprocessing/2_fastqc_chr3_raw.sh
+++ code/01_preprocessing/2_fastqc_chr3_raw.sh
@@ -10,9 +10,24 @@
 # Load module
 module load FastQC
 
-mkdir -p /home/flst8788/Genome-Analysis-1MB462/analysis/01_preprocessing/fastqc_raw_chr3
+mkdir -p /home/flst8788/Genome-Analysis-1MB462/analysis/01_preprocessing/fastqc_raw_chr3/illumina
+mkdir -p /home/flst8788/Genome-Analysis-1MB462/analysis/01_preprocessing/fastqc_raw_chr3/hic
+mkdir -p /home/flst8788/Genome-Analysis-1MB462/analysis/01_preprocessing/fastqc_raw_chr3/nanopore
 
+# Illumina
 fastqc -t 2 \
 /home/flst8788/Genome-Analysis-1MB462/data/raw_data/chr3_illumina_R1.fastq.gz \
 /home/flst8788/Genome-Analysis-1MB462/data/raw_data/chr3_illumina_R2.fastq.gz \
 -o /home/flst8788/Genome-Analysis-1MB462/analysis/01_preprocessing/fastqc_raw_chr3/
+
+# CHR3 Hi-C
+fastqc -t 2 \
+/home/flst8788/Genome-Analysis-1MB462/data/raw_data/chr3_hiC_R1.fastq.gz \
+/home/flst8788/Genome-Analysis-1MB462/data/raw_data/chr3_hiC_R2.fastq.gz \
+-o /home/flst8788/Genome-Analysis-1MB462/analysis/01_preprocessing/fastqc_raw_chr3/hic
+
+# CHR3 NANOPORE
+fastqc -t 2 \
+/home/flst8788/Genome-Analysis-1MB462/data/raw_data/chr3_clean_nanopore.fq.gz \
+-o /home/flst8788/Genome-Analysis-1MB462/analysis/01_preprocessing/fastqc_raw_chr3/nanopore
+
