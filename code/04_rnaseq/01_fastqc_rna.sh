43463b0394ab0c407bbe25520f568b12e77f7021
Florence Stadelmann <flst8788@pelle2.uppmax.uu.se>
2026-04-15 14:32:58 +0200

    created the fastqc script for the rna samples

---
+++ code/04_rnaseq/01_fastqc_rna.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 2
+#SBATCH -t 02:00:00
+#SBATCH --mem=16G
+#SBATCH -J fastqc_rna
+#SBATCH -o ../../logs/slurm-%j_fastqc_rna.out
+
+module load FastQC
+
+INDIR=/home/flst8788/Genome-Analysis-1MB462/data/raw_data/rna
+OUTDIR=/home/flst8788/Genome-Analysis-1MB462/analysis/04_rnaseq/fastqc_rna
+
+mkdir -p $OUTDIR
+
+# Run FastQC on all RNA files
+for file in $INDIR/*.fq.gz
+do
+    fastqc -t 4 -o $OUTDIR $file
+done
