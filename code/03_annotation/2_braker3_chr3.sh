673ba8a46e8960acea39955113f05279bef1f095
Florence Stadelmann <flst8788@pelle1.uppmax.uu.se>
2026-04-17 21:49:16 +0200

    modified the script to do a continuation instead of a full new run

--- code/03_annotation/2_braker3_chr3.sh
+++ code/03_annotation/2_braker3_chr3.sh
@@ -3,7 +3,7 @@
 #SBATCH -A uppmax2026-1-61
 #SBATCH -p pelle
 #SBATCH -c 2
-#SBATCH -t 24:00:00
+#SBATCH -t 12:00:00
 #SBATCH --mem=64G
 #SBATCH -J braker_chr3
 #SBATCH -o /home/flst8788/Genome-Analysis-1MB462/logs/slurm-%j_braker.out
@@ -13,10 +13,10 @@ module load SAMtools/1.22-GCC-13.3.0
 
 # PROJECT PATHS
 GENOME=/home/flst8788/Genome-Analysis-1MB462/analysis/03_annotation/repeatmasker_chr3/pilon_chr3.fasta.masked
-RNA_BAM_DIR=/home/flst8788/Genome-Analysis-1MB462/analysis/04_rnaseq/hisat2_rna
-OUTDIR=/home/flst8788/Genome-Analysis-1MB462/analysis/03_annotation/braker_chr3
+# RNA_BAM_DIR=/home/flst8788/Genome-Analysis-1MB462/analysis/04_rnaseq/hisat2_rna
+OUTDIR=/home/flst8788/Genome-Analysis-1MB462/analysis/03_annotation/braker_chr3_restart
 BRAKER_SIF=/proj/uppmax2026-1-61/Genome_Analysis/2_Zhou_2023/braker3.sif
-PROTEIN_DB=/proj/uppmax2026-1-61/Genome_Analysis/2_Zhou_2023/Ceratodon_purpureus/C_purpureus.faa
+# PROTEIN_DB=/proj/uppmax2026-1-61/Genome_Analysis/2_Zhou_2023/Ceratodon_purpureus/C_purpureus.faa
 
 # FIX TMPDIR
 export TMPDIR=$OUTDIR/tmp
@@ -29,16 +29,16 @@ cd $OUTDIR
 echo "Checking genome..."
 ls -lh $GENOME
 
-echo "Checking BAM files..."
-ls $RNA_BAM_DIR/*.sorted.bam
+# echo "Checking BAM files..."
+# ls $RNA_BAM_DIR/*.sorted.bam
 
 # COLLECT BAM FILES
 
-BAM_FILES=$(ls $RNA_BAM_DIR/*.sorted.bam | tr '\n' ',' | sed 's/,$//')
+# BAM_FILES=$(ls $RNA_BAM_DIR/*.sorted.bam | tr '\n' ',' | sed 's/,$//')
 
-echo "BAM list:"
-echo $BAM_FILES
-
+# echo "BAM list:"
+#echo $BAM_FILES
+ 
 # RUN BRAKER3
 singularity exec \
 -B $HOME/augustus_config:/opt/Augustus/config \
@@ -46,11 +46,11 @@ singularity exec \
 -B /proj/uppmax2026-1-61:/proj/uppmax2026-1-61 \
 $BRAKER_SIF braker.pl \
     --genome=$GENOME \
-    --bam=$BAM_FILES \
-    --prot_seq=$PROTEIN_DB \
     --species=chr3_moss \
-    --softmasking \
-    --threads 2 \
-    --workingdir=$OUTDIR 
+    --workingdir=$OUTDIR \
+    --gmetp_results_dir=/home/flst8788/Genome-Analysis-1MB462/analysis/03_annotation/braker_chr3/GeneMark-ETP \
+    --useexisting \
+    --skipOptimize \
+    --threads 2
 
 echo "BRAKER finished successfully!"
