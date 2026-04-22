02f12cf46df49b22eeee61b61305f392a39973ac
Florence Stadelmann <flst8788@pelle2.uppmax.uu.se>
2026-04-22 15:54:07 +0200

    made the script more robust

--- code/04_rnaseq/4.0_deseq2.R
+++ code/04_rnaseq/4.0_deseq2.R
@@ -4,33 +4,42 @@ library(DESeq2)
 counts <- read.table(
   "/home/flst8788/Genome-Analysis-1MB462/analysis/04_rnaseq/counts/counts.txt",
   header = TRUE,
-  row.names = 1,
   comment.char = "#"
+  stringAsFactors = FALSE
 )
 
 # Remove annotation columns from featureCounts
-counts <- counts[ ,6:ncol(counts)]
+countData <- counts[, -(1:6)]
+rownames(countData) <- counts$Geneid
 
 # Clean column names (remove paths if present)
-colnames(counts) <- gsub(".*/", "", colnames(counts))
-colnames(counts) <- gsub(".sorted.bam", "", colnames(counts))
+colnames(countData) <- basename(colnames(countData))
+colnames(countData) <- sub("\\.sorted\\.bam$", "", colnames(countData))
 
 # Load metadata
 coldata <- read.table(
   "/home/flst8788/Genome-Analysis-1MB462/data/metadata/rna_metadata.tsv",
   header = TRUE,
   row.names = 1
+  stringsAsFactors = FALSE
 )
 
 # Ensure same order
 coldata <- coldata[colnames(counts), ]
 
+if (!all(colnames(countData) == rownames(coldata))) {
+  stop("ERROR: sample names in counts and metadata do not match!")
+}
+
 # Create DESeq dataset
 dds <- DESeqDataSetFromMatrix(
-  countData = counts,
+  countData = round(countData),
   colData = coldata,
   design = ~ condition
 )
+
+# Filter low counts
+dds <- dds[rowSums(counts(dds)) > 10, ]
 
 # Run DESeq2
 dds <- DESeq(dds)
@@ -39,9 +48,10 @@ dds <- DESeq(dds)
 res <- results(dds)
 
 # Save results
+dir.create("/home/flst8788/Genome-Analysis-1MB462/analysis/04_rnaseq/deseq2",
+           showWarnings = FALSE, recursive = TRUE)
+
 write.csv(as.data.frame(res),
           file = "/home/flst8788/Genome-Analysis-1MB462/analysis/04_rnaseq/deseq2/deseq2_results.csv")
 
-# Summary
 summary(res)
-
