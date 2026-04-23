daa69c12081e03f9f4abecf0f91bc168e1636737
Florence Stadelmann <flst8788@pelle1.uppmax.uu.se>
2026-04-23 12:59:20 +0200

    updated the eggnog script to match course

--- code/03_annotation/4_eggnogg_chr3.sh
+++ code/03_annotation/4_eggnogg_chr3.sh
@@ -6,7 +6,7 @@
 #SBATCH -t 18:00:00
 #SBATCH --mem=32G
 #SBATCH -J eggnog_chr3
-#SBATCH -o eggnog_%j.out
+#SBATCH -o /home/flst8788/Genome-Analysis-1MB462/logs/eggnog_%j.out
 
 #Load modules
 module load eggnog-mapper/2.1.13-gfbf-2024a
@@ -24,12 +24,13 @@ cd $OUTDIR
 emapper.py \
     -i $PROTEINS \
     -o chr3_eggnog \
+    -m hmmer \
+    -d viridiplantae \
     --output_dir $OUTDIR \
     --itype proteins \
     --cpu 2 \
     --sensmode ultra-sensitive \
     --override \
     --go_evidence all \
-    --pfam_realign hmm \
-    --usemem \
-    --hmm_mode
+    --pfam_realign realign \
+    --usemem 
