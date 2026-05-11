b238dc7d6716fefdbc279d222c7a46096904974d
FlorenceStad <florence.stadelmann.8788@student.uu.se>
2026-05-11 17:56:53 +0200

    changed enricher

--- code/05_enrichment/1.0_go_enrichment.R
+++ code/05_enrichment/1.0_go_enrichment.R
@@ -45,7 +45,7 @@ go_data <- merged %>%
 
 term2gene <- unique(go_data[, c("GOs", "gene_id")])
 
-ego <- clusterProfiler::enricher(
+ego <- clusterProfiler::enrichGO(
   gene = unique(go_data$gene_id),
   TERM2GENE = term2gene
 )
